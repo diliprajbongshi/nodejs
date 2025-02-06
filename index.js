a0c22573b633fe438a01b1d12ab028cccb7e9e4f
Dilip raj <dr1117255@gmail.com>
2025-02-06 11:00:44 +0600

    add json data

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ require("dotenv").config();
 const express = require('express')
 const app = express()
 const port = process.env.PORT;
-
+const data = require('./data.json')
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/youtube', (req, res) => {
+  res.send('You tube')
+})
+app.get('/data', (req, res) => {
+  res.json(data)
 })
 
 app.listen(port, () => {
